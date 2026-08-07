38ff731809077bbd62508547c2659c192dd0497d
Kadir Akdemir <45338685+kadir001@users.noreply.github.com>
2026-08-07 21:30:40 +0200

    Refactor server.js by cleaning up code
    Removed starter comments and redundant fs import.
    
    Signed-off-by: Kadir Akdemir <45338685+kadir001@users.noreply.github.com>

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,14 +1,5 @@
-// Starter file — add your code here
-const fs = require('fs');
-console.log(fs);
-
 const fs = require('fs');
 
 fs.readFile('assets/poem.txt', { encoding: 'utf8' }, (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
   console.log(data);
 });
