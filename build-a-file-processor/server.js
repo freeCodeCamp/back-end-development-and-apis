80778b2d82eee0b97a251f3b0b3d33bbbfd210df
Kadir Akdemir <45338685+kadir001@users.noreply.github.com>
2026-08-07 21:47:40 +0200

    Replace file reading with synchronous write
    Removed asynchronous file reading and added synchronous write.
    
    Signed-off-by: Kadir Akdemir <45338685+kadir001@users.noreply.github.com>

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,13 +1,3 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const fsPromises = fs.promises;
-
-async function main() {
-  const data = await fsPromises.readFile('assets/poem.txt', {
-    encoding: "utf8",
-  });
-
-  console.log(data);
-}
-
-main();
+fs.writeFileSync('assets/output.txt', 'Hello, freeCodeCamp!');
