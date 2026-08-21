ef6254b381ccdd8cbe3e2107a3cb9cea809a888c
Dario <105294544+Dario-DC@users.noreply.github.com>
2026-08-21 15:28:38 +0200

    define __dirname

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
