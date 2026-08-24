6e4a6877ad777e3815c0fc31260c3e1d43b32e4d
Dario <105294544+Dario-DC@users.noreply.github.com>
2026-08-24 17:13:28 +0200

    use import.meta.dirname

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -1,10 +1,6 @@
 import express from "express";
 import cors from "cors";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
@@ -12,7 +8,7 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 app.use(express.static("public"));
 
 app.get("/", (_req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(import.meta.dirname + "/views/index.html");
 });
 
 // Do not change code above this line
